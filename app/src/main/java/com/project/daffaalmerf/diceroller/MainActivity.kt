be73301771ad302c211f9a4ad298f22968d1689f
Daffa Almer Fauzan <daffaalmerfauzan@gmail.com>
2021-07-30 14:16:54 +0700

    second commit - dice with image

--- app/src/main/java/com/project/daffaalmerf/diceroller/MainActivity.kt
+++ app/src/main/java/com/project/daffaalmerf/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.project.daffaalmerf.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.btnRoll)
-        rollButton.setOnClickListener{ rollDie() }
+        rollButton.setOnClickListener { rollDie() }
 
     }
 
@@ -26,16 +26,36 @@ class MainActivity : AppCompatActivity() {
      * Roll the die and update on the screen with the result
      */
 
-    private fun rollDie(){
+    private fun rollDie() {
         // Create an instance of die with a side of 6
         val dieOne = Die(6)
         val dieTwo = Die(6)
         // Update the screen with the dice roll
-        val dieNumberOne: TextView = findViewById(R.id.txtNumberOne)
-        val dieNumberTwo: TextView = findViewById(R.id.txtNumberTwo)
-        dieNumberOne.setText(dieOne.roll().toString())
-        dieNumberTwo.setText(dieTwo.roll().toString())
-        Toast.makeText(this, "Die rolled!", Toast.LENGTH_SHORT).show()
+        val dieOneImage: ImageView = findViewById(R.id.imgDieOne)
+        val dieTwoImage: ImageView = findViewById(R.id.imgDieTwo)
+
+        val dieOneNumber = dieOne.roll()
+        val dieTwoNumber = dieTwo.roll()
+
+        val dieOneDrawable = changeDrawable(dieOneNumber)
+        val dieTwoDrawable = changeDrawable(dieTwoNumber)
+
+        dieOneImage.setImageResource(dieOneDrawable)
+        dieOneImage.contentDescription = dieOneNumber.toString()
+        dieTwoImage.setImageResource(dieTwoDrawable)
+        dieTwoImage.contentDescription = dieTwoNumber.toString()
+        Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun changeDrawable(number: Int) : Int {
+        when (number) {
+            1 -> return R.drawable.dice_1
+            2 -> return R.drawable.dice_2
+            3 -> return R.drawable.dice_3
+            4 -> return R.drawable.dice_4
+            5 -> return R.drawable.dice_5
+            else -> return R.drawable.dice_6
+        }
     }
 }
 
